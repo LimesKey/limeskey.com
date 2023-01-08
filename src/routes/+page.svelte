4c9346a5192fc0b9e12d711684257ba1f9c73ec8
InspectorRex <kubupaul0@gmail.com>
2023-01-08 16:15:11 +0100

    Updated Background and Code

---
+++ src/routes/+page.svelte
@@ -0,0 +1,44 @@
+<head>
+	<link rel="stylesheet" href="assets/css/background-style.css" />
+	<link
+		href="https://fonts.googleapis.com/css?family=Lato:300,400,700"
+		rel="stylesheet"
+		type="text/css"
+	/>
+</head>
+<body>
+	<div class="background" />
+	<div class="Clouds">
+		<div class="Cloud Foreground" />
+		<div class="Cloud Background" />
+		<div class="Cloud Foreground" />
+		<div class="Cloud Background" />
+		<div class="Cloud Foreground" />
+		<div class="Cloud Background" />
+		<div class="Cloud Background" />
+		<div class="Cloud Foreground" />
+		<div class="Cloud Background" />
+		<div class="Cloud Background" />
+	</div>
+	<main>
+		<div class="home" />
+	</main>
+</body>
+
+<style>
+	main {
+		width: 100%;
+		min-height: 100vh;
+
+		display: grid;
+		place-content: center;
+	}
+
+	.home {
+		width: 250px;
+		height: 250px;
+
+		background-color: gray;
+		border-radius: 25px;
+	}
+</style>
