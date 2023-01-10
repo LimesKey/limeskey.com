60390b22da25a547fd19c6972f05aaea25a39062
LimesKey <85136735+LimesKey@users.noreply.github.com>
2023-01-10 09:36:26 -0500

    formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,9 +6,10 @@
 		type="text/css"
 	/>
 </head>
+
 <body>
 	<div class="background" />
-	<div class="Clouds">
+		<div class="Clouds">
 		<div class="Cloud Foreground" />
 		<div class="Cloud Background" />
 		<div class="Cloud Foreground" />
@@ -20,25 +21,57 @@
 		<div class="Cloud Background" />
 		<div class="Cloud Background" />
 	</div>
+
 	<main>
-		<div class="home" />
+		<div class="home">
+			<style>
+				main {
+					min-width: 100%;
+					min-height: 100vh;
+			
+					display: grid;
+					place-content: center;
+				}
+			
+				.home {
+					min-width: 500px;
+					min-height: 500px;
+			
+					background-color: gray;
+					border-radius: 25px;
+					opacity: 0.8;
+				}
+			</style>
+
+			<background>
+				<style>
+					.background {
+						min-width: 100%;
+						min-height: 100%;
+						display: fill;
+						background-size: cover;
+						position: fixed;
+						background-image: url("/assets/cat.jpg");
+						background-repeat: no-repeat;
+						background-color: aqua;
+						z-index: -2;
+						background-position: 90%, 60%; 
+					}
+
+					.background:after {
+						content: "";
+						width: 100%;
+						height: 100%;
+						backdrop-filter: blur(10px);
+						pointer-events: none;
+						position: fixed;
+						white-space: nowrap;
+						overflow: hidden;
+						display: flex;
+					}
+				</style>
+			</background>
+
+		</div>
 	</main>
 </body>
-
-<style>
-	main {
-		width: 100%;
-		min-height: 100vh;
-
-		display: grid;
-		place-content: center;
-	}
-
-	.home {
-		width: 250px;
-		height: 250px;
-
-		background-color: gray;
-		border-radius: 25px;
-	}
-</style>
